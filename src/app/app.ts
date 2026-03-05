069f8c00b6eb7b8c152a2f3848ed8415a781b651
Tania Kaffashbashi <tania.kaffashbashi@gmail.com>
2026-03-05 16:13:11 +0100

    testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('quizzie');
+  protected inputText = signal('');
+
+  protected onInputChange(value: string) {
+    this.inputText.set(value);
+  }
 }
